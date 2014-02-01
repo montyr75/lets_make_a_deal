d6d8a15f35269b79a7cb16d61ec3bda587f851c9
Monty Rasmussen <montyr75@gmail.com>
2014-02-01 07:16:28 -0700

    program now working

--- bin/lets_make_a_deal.dart
+++ bin/lets_make_a_deal.dart
@@ -8,39 +8,50 @@
 
 import 'dart:math';
 
-//const int TRIALS = 1000000;
-const int TRIALS = 1;
+const int TRIALS = 1000000;
+//const int TRIALS = 1;
 
 int correct = 0;
 int grandPrizeDoor;
 int guessDoor;
+int eliminateDoor;
 
-final List<int> doors = const [1, 2, 3];
+final List<int> doors = const [1, 2, 3];    // list of all doors
+List<int> eliminationDoors;   // list of doors that can be eliminated
 
 Random random = new Random();
 
 void main() {
   for (int i = 0; i < TRIALS; i++) {
-    // randomly place grand prize
+    // randomly place grand prize behind a door
     grandPrizeDoor = random.nextInt(3) + 1;
 
     // randomly guess a door
     guessDoor = random.nextInt(3) + 1;
 
     // construct List of possible doors to eliminate
-    List<int> eliminationDoors = doors.where((int door) => door != grandPrizeDoor && door != guessDoor);
+    eliminationDoors = doors.where((int door) => door != grandPrizeDoor && door != guessDoor).toList(growable: false);
 
     // select randomly from elimination List for door to eliminate
-    int eliminateDoor = eliminationDoors[random.nextInt(eliminationDoors.length)];
+    eliminateDoor = eliminationDoors[random.nextInt(eliminationDoors.length)];
 
     // switch guess
-    guessDoor = doors.where((int door) => door != eliminateDoor && door != guessDoor).first;
+    int newGuessDoor = doors.where((int door) => door != eliminateDoor && door != guessDoor).first;
 
-    // if guess turns out correct, count it
-    if (guessDoor == grandPrizeDoor) {
+    // if new guess turns out correct, count it
+    if (newGuessDoor == grandPrizeDoor) {
       correct++;
     }
-
-    print("The percentage of correct guesses was ${(correct / TRIALS) * 100}%");
+    
+//    print("""
+//      grandPrizeDoor: $grandPrizeDoor
+//      guessDoor: $guessDoor
+//      eliminationDoors: $eliminationDoors
+//      eliminateDoor: $eliminateDoor
+//      newGuessDoor: $newGuessDoor
+//      correct: $correct
+//    """);
   }
+  
+  print("The percentage of correct guesses was ${(correct / TRIALS) * 100}%");
 }
